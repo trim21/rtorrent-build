fb3a42b1875149a43656d84db945c3d40ba65692
trim21 <i@trim21.me>
2026-05-29 15:59:30 +0800

    fix: enable BUILD_STATIC_LIBS for nghttp2

--- src/rtorrent_builder/deps/nghttp2.py
+++ src/rtorrent_builder/deps/nghttp2.py
@@ -4,7 +4,6 @@ from ._cmake import CMakeBuilder
 class Nghttp2Builder(CMakeBuilder):
     def cmake_args(self, prefix: str) -> list[str]:
         return [
-            "-DBUILD_SHARED_LIBS=OFF",
             "-DENABLE_LIB_ONLY=ON",
-            "-DBUILD_TESTING=OFF",
+            "-DBUILD_STATIC_LIBS=ON",
         ]
